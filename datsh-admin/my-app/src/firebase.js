3275c3b1129d8c98712e25789df1dbaa6857576c
Haroun-dev <117525845+Ghazouani-Contact@users.noreply.github.com>
2023-02-01 19:12:16 +0100

    Update firebase.js

--- datsh-admin/my-app/src/firebase.js
+++ datsh-admin/my-app/src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAl8xUWnCDakCjHvSnzYcT3L4-LiSJVAW0",
-    authDomain: "hygieneplus-d9a17.firebaseapp.com",
-    projectId: "hygieneplus-d9a17",
-    storageBucket: "hygieneplus-d9a17.appspot.com",
-    messagingSenderId: "510535975111",
-    appId: "1:510535975111:web:2a693562d5df9ef86d7ee2"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
